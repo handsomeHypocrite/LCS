c1e44ef31d6b045660ce5d3ab2a75fa35ea92844
Bokai Chen <kavenchen@hotmail.co.nz>
2017-11-30 19:56:58 +1300

    added comments

--- LCS.py
+++ LCS.py
@@ -1,8 +1,14 @@
+#Bokai Chen
+#The program asks for 2 inputs from user
+#Returns the LCS of the 2 strings and the length of the LCS
+
 
 s1 = str(input("1st string \n"))
 s2 = str(input("2nd string \n"))
 
+
 def find_matching_character(s2, character):
+    """Function finding a character in a given string and return the index of that character. """
     for index, letter in enumerate(s2):
         if character == letter:
             return index
@@ -12,24 +18,26 @@ def find_matching_character(s2, character):
 
 
 def find_LCS(s1, s2):
-    # print("s1 is: %s" %s1)
-    # print("s2 is: %s" %s2)
+    """Recursive function to find the LCS between 2 input strings recursively """
+
+#The list of common subsequences between 2 strings
     local_CS = []
+
+#Looping through the first string to find a match in the second string.
     for index, character in enumerate(s1):
         # print(character)
         # print(s2)
 
         match = find_matching_character(s2, character)
-        # print(match)
+
+#If matching is found, the right side of the 2 characters of the 2 strings are passed into the function recursively
         if match is not False:
-            # print(character)
-            # print(s1[index+1:])
-            # print(s2[match+1:])
-            # print("----------\n")
             next_LCS = find_LCS(s1[index+1:], s2[match+1:])
+#The returned LCS of the substrings are appended to the mathing characters
             local_CS.append(character + next_LCS)
     if not local_CS:
         return ""
+#The longest of the list of common subsequence is returned
     return max(local_CS, key = len)
 
 
